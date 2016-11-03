7566d2d73a8f6d8028c14197d73fff5c7c2059d5
Tshiamo <tshiamo@Tshiamos-MacBook-Pro.local>
2016-11-03 14:23:34 +0200

    First commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name tutorialApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the tutorialApp
+ */
+angular.module('tutorialApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma',
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name tutorialApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the tutorialApp
+ */
+angular.module('tutorialApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
